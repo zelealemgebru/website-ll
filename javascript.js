bd77e6fa344e5ad10237488c579db950ce38eb26
Zelealem Gebru <123925020+zelealemgebru@users.noreply.github.com>
2023-04-15 03:35:45 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+!DOCTYPE <html>
+<html>
+  <head>
+    <title>My HTML Page </title>
+  </head>
+  <body>
+  <h1>The Importance of an Effective Homepage.</h1>
+  <h2>An effective homepage tells your user who you are and what you do as soon as they set eyes on it. If it is not immediately apparent, we can guarantee that most visitors won’t stick around for long</h2>
+  <h2>It establishes your brand right off the bat. This is your chance to show off your hard work and what your brand is all about. For most users, your homepage will be their first exposure to your business – and as the old saying goes
+   A good headline doesn it have words to waste it needs to be short and clear, and most importantly</h2>
+    <script src="myscript.js"></script>
+
+  </body>
+
+</html>
